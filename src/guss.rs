39e3b7eb261708e8bab38bc6ae2011827fd9a866
siddharth1729 <siddharth.sabron@gmail.com>
2024-01-07 22:26:56 +0530

    Improving guss game code

--- src/guss.rs
+++ src/guss.rs
@@ -1,14 +1,28 @@
 use std::io;
 
 pub fn guessing_game() {
-    println!("\n================= THIS IS A GUESSING GAME! ===================\n");
+    println!(
+        "\n
+
+    ██      ███████  ██████  ██  ██████  ███    ██ 
+    ██      ██      ██       ██ ██    ██ ████   ██ 
+    ██      █████   ██   ███ ██ ██    ██ ██ ██  ██ 
+    ██      ██      ██    ██ ██ ██    ██ ██  ██ ██ 
+    ███████ ███████  ██████  ██  ██████  ██   ████ 
+                                                  
+                                                  \n"
+    );
     println!("Please enter a number:");
 
     let mut input = String::new();
+    let mut guss = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
 
+    io::stdin()
+        .read_line(&mut guss)
+        .expect("Failed to read guss");
     // Parse the input into a u32
     let guessed_num: u32 = match input.trim().parse() {
         Ok(num) => num,
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod guss;
 use guss::guessing_game;
 
 fn main() {
-    //this is a gussing game module called here which is writte in a seprate file 
+    //this is a gussing game module called here which is writte in a seprate file
     guessing_game();
 }
